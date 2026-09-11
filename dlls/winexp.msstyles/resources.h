701435f3c234cd7f9c687b4e0ce4e744933c57da
The412Banner <205237651+The412Banner@users.noreply.github.com>
2026-09-11 10:31:57 -0400

    winexp.msstyles: add a Windows XP style visual style
    Luna-like push buttons, check boxes, radio buttons, group boxes, scroll
    bars, combo box buttons, edit borders, spin buttons, tabs, progress bars,
    trackbars, headers and tree view glyphs, in the blue, olive green and
    silver colour schemes. The bitmaps and the resource script are generated
    by genimages.py. There are no system metrics in it: the theme only
    changes how controls are drawn, never their size or the system colours.

---
+++ dlls/winexp.msstyles/resources.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 The412Banner
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <winnt.h>
+
+/* uxtheme loads the colour names from 1000 + index and their tooltips from 2000 + index */
+#define IDS_COLOR_DISPLAY_NAME_BLUE    1000
+#define IDS_COLOR_DISPLAY_NAME_OLIVE   1001
+#define IDS_COLOR_DISPLAY_NAME_SILVER  1002
+#define IDS_COLOR_TOOLTIP_BLUE         2000
+#define IDS_COLOR_TOOLTIP_OLIVE        2001
+#define IDS_COLOR_TOOLTIP_SILVER       2002
+#define IDS_SIZE_DISPLAY_NAME_NORMAL   3000
+#define IDS_SIZE_TOOLTIP_NORMAL        4000
